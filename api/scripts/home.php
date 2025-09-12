39dcc8929afb2eee2a50d985e6a884cb9b3a0367
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-09-12 17:24:04 -0300

    api consumer

--- api/scripts/home.php
+++ api/scripts/home.php
@@ -1,34 +1,5 @@
 <?php 
 defined ('CONTROL') or die ("Acesso inválido!");
-
-$curl = curl_init();
-
-curl_setopt_array($curl, [
-  CURLOPT_URL => "https://restcountries.com/v3.1/all",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_SSL_VERIFYPEER => false,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => [
-    "Accept: */*",
-
-  ],
-]);
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-}
-
 ?>
 
 <div class="conteiner mt-5">
