e5a8ec54891815840aac8151f20102c8fe446dc6
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-09-12 00:43:59 -0300

    bug

--- login/public/nav.php
+++ login/public/nav.php
@@ -15,8 +15,8 @@
     <a href="?rota=page1">Página 1</a>
     <a href="?rota=page2">Página 2</a>
     <a href="?rota=page3">Página 3</a>
-    <a href="?rota=page3">Página 4</a>
-    <a href="?rota=page3">Página 5</a>
+    <a href="?rota=page4">Página 4</a>
+    <a href="?rota=page5">Página 5</a>
     <a href="?rota=logout">Sair</a>
 </nav>
 
